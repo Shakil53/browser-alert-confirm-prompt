8fb1d08a35f67202173125cc2a87e62b8031ad80
Shakil53 <shakilahmed.dcc@gmail.com>
2022-11-13 23:53:13 +0600

    new

--- alert.js
+++ alert.js
@@ -1,3 +1,7 @@
-const alert_fun = () => {
-    console.log(alert("I am shakil"));
+// function alertFun() {
+//     alert("shakil is here have to say bhoom");
+// }
+
+const alertFun = () => {
+    alert("maa is coming");
 }
