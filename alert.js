274eea3e9dfaa4c74d709489c9312f9a3a9e835e
Shakil53 <shakilahmed.dcc@gmail.com>
2022-11-14 00:26:02 +0600

    done

--- alert.js
+++ alert.js
@@ -3,5 +3,23 @@
 // }
 
 const alertFun = () => {
-    alert("maa is coming");
+    alert("Hey User, Tomorrow we have a exam!!!");
+}
+
+const confirmBtn = () => {
+    const response = confirm("Do you want attend the Exam?");
+    if (response == true) {
+        console.log("Yes I want");
+        alert("Exam Started at 10 o'clock");
+
+    }
+    else {
+        console.log("No I am not prepared");
+        alert("Ok, Prepare for the Next Exam!!")
+    }
+}
+
+const askName = () => {
+    const inputData = prompt("What is Your Name");
+    alert(inputData);
 }
